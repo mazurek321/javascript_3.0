d0ef65a7f797b70cc9c48217ac086830e685dae9
Paweł Ł. <123320714+lewapFT9@users.noreply.github.com>
2024-10-25 17:06:56 +0000

    one

--- script.js
+++ script.js
@@ -19,6 +19,21 @@
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
       .then((array) => {
+        answer.innerHTML = "";
+        array.forEach(function (element) {
+          answer.innerHTML += `<strong>${element.id} - <span>${element.title}</span></strong>                                    <p>${element.body}</p>`;
+        });
+      });
+      });
+    
+
+  });
+
+  cw2.addEventListener("click", function () {
+    answer.innerHTML = "Loading...";
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((array) => {
         const element = array.find((element) => element.id === 5);
 
         answer.innerHTML = "";
@@ -28,12 +43,10 @@
         } else {
           answer.innerHTML = "Post not found";
         }
-      });
-    
-
+    //TODO
   });
 
-  cw2.addEventListener("click", function () {
+  cw3.addEventListener("click", function () {
     answer.innerHTML = "Processing...";
 
     const newPost = {
@@ -55,10 +68,6 @@
         answer.innerHTML = `Dodano nowy post o ID = ${newPost.id}`;
       });
     console.log(`Dodano nowy post o ID = ${newPost.id}`)
-    //TODO
-  });
-
-  cw3.addEventListener("click", function () {
     //TODO
   });
 })();
